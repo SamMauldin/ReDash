1655667cd3a0d43f46efdc892f8cc177a02554c3
Sam Mauldin <sam@fluidnode.com>
2016-09-20 12:53:24 -0500

    Add system for handling events
    TODO: Make sure events are the same on client + server

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const http = require("http");
+"use strict";
+
+const colors = require("colors");
+
+console.log("Starting ReDash".cyan);
+
 const express = require("express");
 const app = express();
+
+const http = require("http");
 const server = http.Server(app);
-const io = require("socket.io")(server);
+
+const Socket = require("./lib/socket");
+
+const socket = new Socket(server);
+
+
+const port = 8080;
+
+server.listen(port);
+console.log(("Listening on port " + port).green);
